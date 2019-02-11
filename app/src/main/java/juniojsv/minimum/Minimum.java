27271b6b4724c1477e468e82efe51b9496aba0e8
JunioJsv <juniojsv@gmail.com>
2019-02-11 16:02:54 -0300

    update

--- app/src/main/java/juniojsv/minimum/Minimum.java
+++ app/src/main/java/juniojsv/minimum/Minimum.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,7 +27,7 @@ public class Minimum extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.minimun);
+        setContentView(R.layout.minimum);
 
         adapter =  new Adapter(this, appsList);
         appsListView = findViewById(R.id.appsListView);
