192b06750baf05cd96b175d7113be0e55e2bf544
JunioJsv <juniojsv@gmail.com>
2024-02-05 18:21:28 -0300

    refactor: set dark mode has default, and set preferences.xml defaults values on start app

--- app/src/main/java/juniojsv/minimum/MinimumActivity.kt
+++ app/src/main/java/juniojsv/minimum/MinimumActivity.kt
@@ -22,6 +22,7 @@ class MinimumActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenc
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false)
         preferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
         preferences.registerOnSharedPreferenceChangeListener(this)
         appearanceHandler(preferences)
